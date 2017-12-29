ee64cfa98fc3812c23ca66017a733bb2d2accae2
dorukgezici <dorukgezici@gmail.com>
2017-12-29 04:57:58 +0300

    v0.9.0

--- bot.py
+++ bot.py
@@ -1,13 +1,11 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import cleverbot
 
-cb = cleverbot.Cleverbot('CC4zfUgbrPndTHT3tLdz3q9-2DA')
-
 import logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 def start(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
+    bot.send_message(chat_id=update.message.chat_id, text="Yazın konuşalım beybiler ;)")
 
 def say(bot, update):
     reply = cb.say(update.message.text)
@@ -17,20 +15,24 @@ def echo(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text=update.message.text)
 
 def unknown(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text="Sorry, I didn't understand that command.")
+    bot.send_message(chat_id=update.message.chat_id, text="Bu komutu bilmiyorum.")
 
 if __name__ == '__main__':
+    cb = cleverbot.Cleverbot('CC4zfUgbrPndTHT3tLdz3q9-2DA')
     updater = Updater(token='506075389:AAHhc04sGoAwXS97dQjx53oi3_OsyIyaAqw')
     dispatcher = updater.dispatcher
 
-    say_handler = CommandHandler('say', say)
-    dispatcher.add_handler(say_handler)
-
-    start_handler = CommandHandler('start', start)
+    start_handler = CommandHandler("start", start)
     dispatcher.add_handler(start_handler)
 
-    echo_handler = MessageHandler(Filters.text, echo)
+    say_handler = CommandHandler("say", say)
+    dispatcher.add_handler(say_handler)
+
+    echo_handler = CommandHandler("echo", echo)
     dispatcher.add_handler(echo_handler)
+
+    default_handler = MessageHandler(Filters.text, say)
+    dispatcher.add_handler(default_handler)
 
     unknown_handler = MessageHandler(Filters.command, unknown)
     dispatcher.add_handler(unknown_handler)
